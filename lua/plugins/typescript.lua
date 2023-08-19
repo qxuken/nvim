fb89b0a6a6fd38703721e7c19621c9ee2d19b15c
Viktor Nasonov <qxuken@gmail.com>
2023-08-19 19:51:56 +0300

    Added js linter/formatter plugins

--- lua/plugins/typescript.lua
+++ lua/plugins/typescript.lua
@@ -56,6 +56,23 @@ return {
     end,
   },
   {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = { eslint = {} },
+      setup = {
+        eslint = function()
+          require("lazyvim.util").on_attach(function(client)
+            if client.name == "eslint" then
+              client.server_capabilities.documentFormattingProvider = true
+            elseif client.name == "tsserver" then
+              client.server_capabilities.documentFormattingProvider = false
+            end
+          end)
+        end,
+      },
+    },
+  },
+  {
     "mfussenegger/nvim-dap",
     optional = true,
     dependencies = {
