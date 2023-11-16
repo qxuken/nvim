b6dce46bd9234f511a231dd32a8d9aa54fe85424
Nasonov V <qxuken@gmail.com>
2023-11-16 12:04:04 +0300

    Fixed indent plugin

--- init.lua
+++ init.lua
@@ -128,10 +128,8 @@ require('lazy').setup({
     'lukas-reineke/indent-blankline.nvim',
     -- Enable `lukas-reineke/indent-blankline.nvim`
     -- See `:help indent_blankline.txt`
-    opts = {
-      char = '┊',
-      show_trailing_blankline_indent = false,
-    },
+    main = 'ibl',
+    opts = {},
   },
 
   -- "gc" to comment visual regions/lines
