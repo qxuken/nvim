cbd0f4fa4a043c50ee177d8e3b239e85ae41bc02
Nasonov V <qxuken@qxc-mbp-m3.local>
2024-08-15 19:13:59 +0300

    Added scratch

---
+++ lua/qxuken/plugins/scratch.lua
@@ -0,0 +1,70 @@
+return {
+  {
+    'LintaoAmons/scratch.nvim',
+    event = 'VeryLazy',
+    dependencies = {
+      { 'ibhagwan/fzf-lua' },
+      { 'nvim-telescope/telescope.nvim' },
+    },
+    keys = {
+      { '<M-C-n>', '<cmd>Scratch<cr>', desc = 'Scratch' },
+      { '<M-C-o>', '<cmd>ScratchOpen<cr>', desc = 'Scratch open' },
+    },
+    config = function()
+      require('scratch').setup {
+        scratch_file_dir = vim.fn.stdpath 'cache' .. '/scratch.nvim', -- where your scratch files will be put
+        window_cmd = 'rightbelow edit', -- 'vsplit' | 'split' | 'edit' | 'tabedit' | 'rightbelow vsplit'
+        use_telescope = true,
+        -- fzf-lua is recommanded, since it will order the files by modification datetime desc. (require rg)
+        file_picker = 'fzflua', -- "fzflua" | "telescope" | nil
+        filetypes = { 'lua', 'js', 'sh', 'ts' }, -- you can simply put filetype here
+        filetype_details = { -- or, you can have more control here
+          json = {}, -- empty table is fine
+          ['yaml'] = {},
+          go = {
+            requireDir = true, -- true if each scratch file requires a new directory
+            filename = 'main', -- the filename of the scratch file in the new directory
+            content = { 'package main', '', 'func main() {', '  ', '}' },
+            cursor = {
+              location = { 4, 2 },
+              insert_mode = true,
+            },
+          },
+        },
+        localKeys = {
+          {
+            filenameContains = { 'js', 'ts' },
+            LocalKeys = {
+              {
+                cmd = '<CMD>%SnipRun<CR>',
+                key = '<M-C-f>',
+                modes = { 'n', 'i', 'v' },
+              },
+            },
+          },
+          {
+            filenameContains = { 'sh' },
+            LocalKeys = {
+              {
+                cmd = '<CMD>RunShellCurrentLine<CR>',
+                key = '<C-r>',
+                modes = { 'n', 'i', 'v' },
+              },
+            },
+          },
+        },
+      }
+    end,
+  },
+
+  {
+    'michaelb/sniprun',
+    branch = 'master',
+    build = 'sh install.sh',
+    opts = {
+      display = {
+        'Terminal',
+      },
+    },
+  },
+}
