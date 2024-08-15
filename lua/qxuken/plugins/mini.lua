a719a98dc5574dc979c9857777b5437394412fc7
Nasonov V <qxuken@qxc-mbp-m3.local>
2024-08-15 15:26:52 +0300

    Added mini plugins(pairs, jump, move)

--- lua/qxuken/plugins/mini.lua
+++ lua/qxuken/plugins/mini.lua
@@ -17,6 +17,12 @@ return {
       -- - sr)'  - Surround Replace ) '
       require('mini.surround').setup()
 
+      require('mini.pairs').setup()
+
+      require('mini.jump').setup()
+
+      require('mini.move').setup()
+
       local statusline = require 'mini.statusline'
       statusline.setup { use_icons = vim.g.have_nerd_font }
       ---@diagnostic disable-next-line: duplicate-set-field
