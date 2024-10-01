d2c54fcf5755a5009bfee63bcf28ed2c96bd544e
Victor Nasonov <qxuken@gmail.com>
2024-10-01 21:51:29 +0300

    Changed theme to cyberdream

--- lua/qxuken/plugins/theme.lua
+++ lua/qxuken/plugins/theme.lua
@@ -1,37 +1,50 @@
 return {
   {
-    'catppuccin/nvim',
-    name = 'catppuccin',
+    'scottmckendry/cyberdream.nvim',
+    lazy = false,
     priority = 1000,
     config = function()
-      require('catppuccin').setup {
-        transparent_background = true,
-        term_colors = true,
-        integrations = {
-          cmp = true,
-          dap = true,
-          gitsigns = true,
-          nvimtree = true,
-          native_lsp = {
-            enabled = true,
-          },
-          mason = true,
-          which_key = true,
-          notify = true,
-          treesitter = true,
-          treesitter_context = true,
-          mini = {
-            enabled = true,
-          },
-          telescope = {
-            enabled = true,
-          },
-          neotest = true,
-        },
+      require('cyberdream').setup {
+        transparent = true,
+        borderless_telescope = false,
       }
 
-      vim.cmd.colorscheme 'catppuccin'
-      vim.cmd.hi 'Comment gui=none'
+      vim.cmd.colorscheme 'cyberdream'
     end,
   },
+  -- {
+  --   'catppuccin/nvim',
+  --   name = 'catppuccin',
+  --   priority = 1000,
+  --   config = function()
+  --     require('catppuccin').setup {
+  --       transparent_background = true,
+  --       term_colors = true,
+  --       integrations = {
+  --         cmp = true,
+  --         dap = true,
+  --         gitsigns = true,
+  --         nvimtree = true,
+  --         native_lsp = {
+  --           enabled = true,
+  --         },
+  --         mason = true,
+  --         which_key = true,
+  --         notify = true,
+  --         treesitter = true,
+  --         treesitter_context = true,
+  --         mini = {
+  --           enabled = true,
+  --         },
+  --         telescope = {
+  --           enabled = true,
+  --         },
+  --         neotest = true,
+  --       },
+  --     }
+  --
+  --     vim.cmd.colorscheme 'catppuccin'
+  --     vim.cmd.hi 'Comment gui=none'
+  --   end,
+  -- },
 }
