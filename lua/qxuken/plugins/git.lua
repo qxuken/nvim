a3d832c647b5bfe3da10952ec5fea3b87fcbfff5
Nasonov Viktor <qxuken@QXC-MPPC.>
2024-10-29 00:38:32 +0300

    feat(git): add keymaps

--- lua/qxuken/plugins/git.lua
+++ lua/qxuken/plugins/git.lua
@@ -1,8 +1,19 @@
 return {
   {
+    'sindrets/diffview.nvim',
+    keys = {
+      { '<leader>bh', '<cmd>DiffviewFileHistory %<CR>', desc = 'Git history' },
+      { '<leader>wd', '<cmd>DiffviewOpen<CR>', desc = 'Git diff' },
+      { '<leader>wm', '<cmd>DiffviewOpen master...HEAD<CR>', desc = 'Git diff with main' },
+    },
+    opts = {},
+  },
+  {
     'NeogitOrg/neogit',
     keys = {
-      { '<leader>bg', '<cmd>Neogit<CR>', desc = 'Open neogit' },
+      { '<leader>bg', '<cmd>Neogit<CR>', desc = 'Neogit' },
+      { '<leader>bl', '<cmd>NeogitLogCurrent<CR>', desc = 'File git log' },
+      { '<leader>wc', '<cmd>NeogitCommit<CR>', desc = 'Git commit' },
     },
     dependencies = {
       'nvim-lua/plenary.nvim',
@@ -13,12 +24,15 @@ return {
   },
   {
     'lewis6991/gitsigns.nvim',
-    lazy = false,
     keys = {
       { ']h', '<cmd>Gitsigns next_hunk<CR>', desc = 'Next git hunk' },
       { '[h', '<cmd>Gitsigns prev_hunk<CR>', desc = 'Prev git hunk' },
-      -- { '<leader>hr', '<cmd>Gitsigns reset_hunk<CR>', desc = 'Git hunk reset' },
-      { '<leader>hd', '<cmd>Gitsigns diffthis<CR>', desc = 'Git hunk diff' },
+      { '<leader>hrr', '<cmd>Gitsigns reset_hunk<CR>', desc = 'Git hunk reset' },
+      -- { '<leader>hd', '<cmd>Gitsigns diffthis<CR>', desc = 'Git hunk diff' },
+      { '<leader>ds', '<cmd>Gitsigns stage_buffer<cr>', desc = 'git stage buffer' },
+      { '<leader>hs', '<cmd>Gitsigns stage_hunk<cr>', desc = 'git stage hunk' },
+      { '<leader>hu', '<cmd>Gitsigns undo_stage_hunk<cr>', desc = 'git unstage hunk' },
+      { '<leader>hv', '<cmd>Gitsigns select_hunk<cr>', desc = 'git select hunk' },
     },
     opts = {
       signs = {
