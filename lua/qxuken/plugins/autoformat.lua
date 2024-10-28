a54bf73b11ddb6de959473ab25493073b240122b
Nasonov Viktor <qxuken@QXC-MPPC.>
2024-10-28 18:39:18 +0300

    feat(autoformat): add ts formaters

--- lua/qxuken/plugins/autoformat.lua
+++ lua/qxuken/plugins/autoformat.lua
@@ -15,9 +15,6 @@ return {
     opts = {
       notify_on_error = false,
       format_on_save = function(bufnr)
-        -- Disable "format_on_save lsp_fallback" for languages that don't
-        -- have a well standardized coding style. You can add additional
-        -- languages here or re-enable it for the disabled ones.
         local disable_filetypes = { c = true, cpp = true }
         return {
           timeout_ms = 500,
@@ -27,7 +24,8 @@ return {
       formatters_by_ft = {
         lua = { 'stylua' },
         go = { 'goimports', 'gofmt' },
-        javascript = { 'prettierd', 'prettier', 'eslint', stop_after_first = true },
+        javascript = { 'eslint_d', 'prettierd', 'prettier', stop_after_first = true },
+        typescript = { 'eslint_d', 'prettierd', 'prettier', stop_after_first = true },
         css = { 'prettierd', 'prettier' },
         templ = { 'templ' },
       },
