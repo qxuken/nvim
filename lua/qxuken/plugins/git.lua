16da2605956dff0ae8fc4c61c6e66ba3ca43ba53
Nasonov Viktor <qxuken@QXC-MPPC.>
2024-11-07 22:03:49 +0300

    fix(gitsigns): set lazy = false

--- lua/qxuken/plugins/git.lua
+++ lua/qxuken/plugins/git.lua
@@ -24,6 +24,7 @@ return {
   },
   {
     'lewis6991/gitsigns.nvim',
+    lazy = false,
     keys = {
       { ']h', '<cmd>Gitsigns next_hunk<CR>', desc = 'Next git hunk' },
       { '[h', '<cmd>Gitsigns prev_hunk<CR>', desc = 'Prev git hunk' },
