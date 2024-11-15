5aa436805074b6093b6dc613515c8ebcec01d3b7
Nasonov Viktor <qxuken@QXC-MPPC.>
2024-11-15 15:07:54 +0300

    feat(ts): remove nu

--- lua/qxuken/plugins/treesitter.lua
+++ lua/qxuken/plugins/treesitter.lua
@@ -1,12 +1,9 @@
 return {
-  { 'LhKipp/nvim-nu', opts = { use_lsp_features = false } },
   {
     'nvim-treesitter/nvim-treesitter',
-    dependencies = { 'LhKipp/nvim-nu' },
     build = ':TSUpdate',
     opts = {
-      ensure_installed = { 'bash', 'c', 'html', 'lua', 'luadoc', 'markdown', 'vim', 'vimdoc', 'nu' },
-      -- Autoinstall languages that are not installed
+      ensure_installed = { 'bash', 'c', 'html', 'lua', 'luadoc', 'markdown', 'vim', 'vimdoc' },
       auto_install = true,
       highlight = {
         enable = true,
@@ -15,17 +12,8 @@ return {
       indent = { enable = true, disable = { 'ruby' } },
     },
     config = function(_, opts)
-      -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
-
       ---@diagnostic disable-next-line: missing-fields
       require('nvim-treesitter.configs').setup(opts)
-
-      -- There are additional nvim-treesitter modules that you can use to interact
-      -- with nvim-treesitter. You should go explore a few and see what interests you:
-      --
-      --    - Incremental selection: Included, see `:help nvim-treesitter-incremental-selection-mod`
-      --    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
-      --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
     end,
   },
 
