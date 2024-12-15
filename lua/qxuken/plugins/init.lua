d018e07be3a48cd8e63cda58b3ffa2009650bdc7
Victor Nasonov <qxuken@gmail.com>
2024-12-15 16:43:01 +0300

    feat(todo): Add keymap for todo search

--- lua/qxuken/plugins/init.lua
+++ lua/qxuken/plugins/init.lua
@@ -5,7 +5,13 @@ return {
   -- "gc" to comment visual regions/lines
   { 'numToStr/Comment.nvim', opts = {} },
 
-  { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
+  {
+    'folke/todo-comments.nvim',
+    event = 'VimEnter',
+    keys = { { '<leader>st', '<cmd>TodoTelescope keywords=TODO<cr>', desc = 'Search TODO' } },
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    opts = { signs = false },
+  },
 
   -- Effortlessly open the URL under the cursor
   {
