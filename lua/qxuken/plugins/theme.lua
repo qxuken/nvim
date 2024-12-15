1d4549000294b0473f7b151e0beafed52e464788
Nasonov Viktor <qxuken@QXC-MPPC.>
2024-12-16 00:22:40 +0300

    feat(theme): set to rose pine

--- lua/qxuken/plugins/theme.lua
+++ lua/qxuken/plugins/theme.lua
@@ -1,19 +1,14 @@
 return {
-  {
-    'EdenEast/nightfox.nvim',
-    lazy = false,
-    priority = 1000,
-    config = function()
-      require('nightfox').setup {
-        transparent = true,
-        palettes = {
-          all = {
-            bg1 = 'NONE',
-          },
-        },
-      }
-
-      vim.cmd 'colorscheme nightfox'
-    end,
-  },
+  'rose-pine/neovim',
+  name = 'rose-pine',
+  config = function()
+    require('rose-pine').setup {
+      variant = 'main',
+      dark_variant = 'dawn',
+      styles = {
+        transparency = false,
+      },
+    }
+    vim.cmd 'colorscheme rose-pine'
+  end,
 }
