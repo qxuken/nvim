4e34b97c1ed727056eacf08a06d09ed7a2a522ba
Victor Nasonov <qxuken@gmail.com>
2024-12-21 14:59:32 +0300

    tweak(conform): change format from on_save to after_save

--- lua/qxuken/plugins/autoformat.lua
+++ lua/qxuken/plugins/autoformat.lua
@@ -14,11 +14,10 @@ return {
     },
     opts = {
       notify_on_error = false,
-      format_on_save = function(bufnr)
+      format_after_save = function(bufnr)
         local disable_filetypes = { c = true, cpp = true, html = true }
         return {
-          timeout_ms = 5000,
-          -- async = true,
+          timeout_ms = 7000,
           lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
         }
       end,
