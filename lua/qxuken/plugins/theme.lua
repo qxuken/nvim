3b2a63da339237b0071ed2cacd82defde6f19ebe
Nasonov V <qxuken@QXC-MBP-M3.local>
2024-12-21 19:00:09 +0300

    chore(theme): move hi groups to theme setup

--- lua/qxuken/plugins/theme.lua
+++ lua/qxuken/plugins/theme.lua
@@ -8,7 +8,18 @@ return {
         bold = true,
         transparency = false,
       },
+      highlight_groups = {
+        LspReferenceText = { fg = 'text', bg = 'leaf', blend = 20, inherit = false },
+        LspReferenceRead = { fg = 'base', bg = 'leaf', inherit = false },
+        LspReferenceWrite = { fg = 'text', bg = 'leaf', blend = 20, inherit = false },
+        NotifyERRORBody = { link = 'NormalFloat' },
+        NotifyWARNBody = { link = 'NormalFloat' },
+        NotifyINFOBody = { link = 'NormalFloat' },
+        NotifyDEBUGBody = { link = 'NormalFloat' },
+        NotifyTRACEBody = { link = 'NormalFloat' },
+      },
     }
+
     vim.cmd 'colorscheme rose-pine'
   end,
 }
