405ddd0dc96049ba10323744126df42be53bccbd
Nasonov Viktor <qxuken@gmail.com>
2025-01-07 00:30:01 +0300

    feat(autoformat): add toggle

--- lua/qxuken/plugins/autoformat.lua
+++ lua/qxuken/plugins/autoformat.lua
@@ -14,7 +14,10 @@ return {
     },
     opts = {
       notify_on_error = false,
-      format_after_save = function(bufnr)
+      format_on_save = function(bufnr)
+        if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+          return
+        end
         local disable_filetypes = { c = true, cpp = true, html = true }
         return {
           timeout_ms = 7000,
@@ -31,6 +34,38 @@ return {
         sql = { 'sqlfmt', stop_after_first = true },
       },
     },
+    init = function()
+      vim.api.nvim_create_autocmd('User', {
+        pattern = 'VeryLazy',
+        callback = function()
+          Snacks.toggle
+            .new({
+              id = 'global_autoformat',
+              name = 'Global autoformat',
+              get = function()
+                return not vim.g.disable_autoformat
+              end,
+              set = function()
+                vim.g.disable_autoformat = not vim.g.disable_autoformat
+              end,
+            })
+            :map '<leader>wf'
+
+          Snacks.toggle
+            .new({
+              id = 'buffer_autoformat',
+              name = 'Buffer autoformat',
+              get = function()
+                return not vim.b.disable_autoformat
+              end,
+              set = function()
+                vim.b.disable_autoformat = not vim.b.disable_autoformat
+              end,
+            })
+            :map '<leader>bf'
+        end,
+      })
+    end,
   },
 
   {
