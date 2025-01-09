430b20bfe146f33c3b73dfb7a298504d17d0451e
Nasonov Viktor <qxuken@QXC-MPPC.>
2025-01-09 20:29:09 +0300

    fix(listchars): set tab to space

--- lua/globals.lua
+++ lua/globals.lua
@@ -61,7 +61,12 @@ vim.opt.splitbelow = true
 vim.opt.list = true
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
-vim.opt.listchars = { tab = vim.g.have_nerd_font and '󰇥 ' or '» ', trail = '·', nbsp = '␣' }
+vim.opt.listchars = {
+  -- tab = vim.g.have_nerd_font and '󰇥 ' or '» ',
+  tab = '  ',
+  trail = '·',
+  nbsp = '␣',
+}
 
 -- Preview substitutions live, as you type!
 vim.opt.inccommand = 'split'
