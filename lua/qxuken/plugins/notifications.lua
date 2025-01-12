602ccc87fbb5552b6c6c0034b586567f14cbea13
Nasonov Viktor <qxuken@QXC-MPPC.>
2025-01-12 15:38:52 +0300

    feat(notify): set fidget.nvim as notify

--- lua/qxuken/plugins/notifications.lua
+++ lua/qxuken/plugins/notifications.lua
@@ -1,17 +1,19 @@
 return {
   {
-    'rcarriga/nvim-notify',
-    event = 'VeryLazy',
-    config = function()
-      require('notify').setup { render = 'minimal', fps = 144 }
-      vim.notify = require 'notify'
-    end,
-  },
-
-  {
     'j-hui/fidget.nvim',
+    event = 'VeryLazy',
+    dependencies = {
+      {
+        'rcarriga/nvim-notify',
+        config = function()
+          require('notify').setup { render = 'minimal', fps = 144 }
+          vim.notify = require 'notify'
+        end,
+      },
+    },
     opts = {
       notification = {
+        override_vim_notify = true,
         window = {
           winblend = 0,
         },
